def5c74a75ab314d93b41e6053cb7df801a58d03
Jalter-ego <jhademels123@gmail.com>
2024-09-02 01:09:15 -0400

    ...

--- config/pg.js
+++ config/pg.js
@@ -1,7 +1,8 @@
 import pg from 'pg';
 
 const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 })
 
 export default pool  
