edc16dca19b69086bb6f70cb27a7f33bd88e425b
Jalter-ego <jhademels123@gmail.com>
2024-09-02 23:52:05 -0400

    ...

--- rutas/usuarioRutas.js
+++ rutas/usuarioRutas.js
@@ -8,7 +8,7 @@ autenticacion.use(express.json())
 autenticacion.post("/registrarse", registerHandler)
 
 autenticacion.get('/', async (req, res) => {
-    const result = await pool.query('select now()')
+    const result = await pool.query('SELECT * FROM usuarios WHERE ci = $1', ['127'])
     return res.json(result.rows[0])
 })
 //inicio de sesion
