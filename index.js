337bf5f6e522436365d93817a8aee4faa4857d78
Jalter-ego <jhademels123@gmail.com>
2024-09-07 11:07:51 -0400

    pruebas con la app web

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import express from 'express';
 import { autenticacion } from './rutas/usuarioRutas.js';
 import { rolesRutas } from './rutas/rolRutas.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173', // Permitir solicitudes desde este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    credentials: true, // Si necesitas enviar cookies o credenciales
+}));
+
 app.use(express.json());
 
 app.use(autenticacion)
