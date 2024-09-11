72ff90c25ea0da84bf8f3d6d8a6c6fb57deffa76
JhoelRodas <rodasjhoel@gmail.com>
2024-09-11 17:28:25 -0400

    Validacion Registro de usuario

--- rutas/usuarioRutas.js
+++ rutas/usuarioRutas.js
@@ -1,11 +1,13 @@
 import express from "express";
 import pool from "../config/pg.js";
 import { registerHandler, loginHandler, eliminarUsuario } from '../controladores/usuarioController.js'
+import {validateCreate} from '../validators/users.js'
 export const autenticacion = express.Router()//creando un enrutador
+
 
 autenticacion.use(express.json())
 
-autenticacion.post("/usuarios/registrarse", registerHandler)
+autenticacion.post("/usuarios/registrarse",validateCreate, registerHandler)
 autenticacion.post("/usuarios/login", loginHandler)
 autenticacion.post("/usuarios/eliminar", eliminarUsuario)
 
