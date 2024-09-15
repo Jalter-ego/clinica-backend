388a99adc207582ce56cb7750a664a1b2595ad35
Jalter-ego <jhademels123@gmail.com>
2024-09-15 08:39:26 -0400

    se agrego controladores en rutas y permisos

--- controladores/permisoController.js
+++ controladores/permisoController.js
@@ -15,3 +15,45 @@ export const crearPermiso = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 }
+
+export const eliminarPermiso = async (req, res) => {
+    try {
+        const { nombre } = req.body
+        const result = await RepositorioPermiso.eliminar({ nombre })
+
+        if (result.error) {
+            return res.status(400).json({ msg: result.error });
+        }
+
+        res.json({ msg: result.msg })
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+export const editarPermiso = async (req, res) => {
+    try {
+        const { newNombre, id } = req.body
+        const result = await RepositorioPermiso.editar({ newNombre, id })
+
+        if (result.error) {
+            return res.status(400).json({ msg: result.error });
+        }
+
+        res.json(result.permiso)
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+export const obtenerPermisos = async (req, res) => {
+    try {
+        const result = await RepositorioPermiso.getPermisos()
+        if (result.error) {
+            return res.status(400).json({ mss: result.error })
+        }
+        res.json(result.permisos)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
